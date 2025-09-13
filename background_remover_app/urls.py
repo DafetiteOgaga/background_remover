b6de7ee504e845a7993ab3121e60b4345a5f4a13
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-14 00:36:40 +0100

    urls.py:

--- background_remover_app/urls.py
+++ background_remover_app/urls.py
@@ -5,7 +5,8 @@ app_name = "background_remover_app"
 
 urlpatterns = [
 	# Create your urlpatterns here.
-	path("", views.test_api),
+	path("", views.test_api, name="test-api"),
+	path("s-test/", views.simple_test, name="simple-test"),
 	path("background-remover/", views.remove_background, name="remove-background"),
     path("background-remover/bulk/", views.remove_background_bulk, name="remove-background-bulk"),
     path("background-remover/bulk-wurl/", views.remove_background_bulk_wurl, name="remove-background-bulk-wurl"),
