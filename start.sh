56b073f1c0d74a9096aef7786288d99fe051b3c3
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-14 00:37:07 +0100

    start.sh:

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#!/bin/bash
 # Start Django app using gunicorn
 # Listen on the port Render provides via $PORT
+# PORT=${PORT:-8001}
 echo "PORT provided is $PORT"
 exec gunicorn background_remover_project.wsgi:application \
     --bind 0.0.0.0:$PORT \
